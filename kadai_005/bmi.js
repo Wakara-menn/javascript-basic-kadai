432bfc9835d68e5bdf7ec207c63418034ecfcffa
Wakaba <Wyamamoto0621@gmail.com>
2023-10-25 22:34:32 +0900

    kadai_005を編集

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,8 +1,7 @@
-// 変数の宣言
-let bodyMassindex;
-
-// 値の代入
-bodyMassindex = (68 / (1.7 * 1.7))
+// 変数の宣言・値の代入
+let userWeight = 68;
+let userHeight = 1.7;
+let userBmi = ( userWeight/ (userHeight * userHeight))
 
 //　コンソールへの出力
-console.log(bodyMassindex);
+console.log(userBmi);
