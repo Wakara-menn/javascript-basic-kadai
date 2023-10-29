515b185dfb90dd3c9bfc9b7282f5f370c6a99d2b
Wakaba <Wyamamoto0621@gmail.com>
2023-10-29 13:24:39 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date("September 22, 2023");
+
+const year = today.getFullYear();
+
+const month = today.getMonth();
+
+const date = today.getDate();
+
+console.log(year + '年' + month + '月' + date + '日');
