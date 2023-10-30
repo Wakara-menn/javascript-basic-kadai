0839fbf5825146e8cd95b842bf4d3807c7c6e822
Wakaba <Wyamamoto0621@gmail.com>
2023-10-31 00:28:49 +0900

    kadai_009を編集

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,18 +1,18 @@
 // 配列の宣言と値の代入を行う
-const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+let holidays = Math.floor(Math.random() * 16);
+
+holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
 // 配列の値の出力をする
-console.log(holidays);
+console.log(holidays[1]);
 
-for(let i = 1; i <= 15; i++) {
-    console.log(i);
+for(let i = 1; i <= 16; i++) {
+    console.log(holidays[i]);
 }
 
-// 変数holidaysの値が0以外である間、変数holidaysの値を出力し続ける
-while(holidays !== 0){
-    holidays = Math.floor(Math.random() * 14);
-    console.log(holidays);
+while(holidays){
+    holidays = Math.floor(Math.random() * 16);
+    if(holidays[i] + 1){
+        console.log(holidays);
+    }
 }
-
-
-
