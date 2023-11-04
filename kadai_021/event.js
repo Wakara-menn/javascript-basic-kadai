8bd0a640e656fac73cf16c0b6261ec6d08660536
Wakaba <Wyamamoto0621@gmail.com>
2023-11-05 01:35:46 +0900

    kadai_021を追加

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// htmL要素がクリックされた時にイベント処理を実行する
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        // textを「ボタンをクリックしてください」から「しました」に変更
+        text.textContent = 'ボタンをクリックしました';        
+    }, 2000);
+});
